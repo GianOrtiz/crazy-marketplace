73ba113905442c1849d422641ee94f80e42b2c51
Gian Ortiz <gian.ortiz@voltbras.com.br>
2023-06-18 09:41:44 -0300

    Add query examples

---
+++ query_examples.sql
@@ -0,0 +1,39 @@
+-- Média de venda de itens e preços por cidades.
+SELECT
+    AVG("quantidade") AS media_quantidade,
+    AVG("valor") AS media_valor,
+    "cidade"
+FROM
+    "Compra"
+LEFT JOIN
+    "Endereco"
+ON 
+    "Compra"."codEndereco"="Endereco"."codEndereco"
+GROUP BY 
+    "cidade";
+
+-- Valor total de venda de produtos por mês.
+SELECT
+    SUM("valor") AS total,
+    "nome"
+FROM 
+    "Compra"
+RIGHT JOIN
+    "Produto"
+ON 
+    "Compra"."codProduto"="Produto"."codProduto"
+GROUP BY
+    "Produto"."codProduto";
+
+-- Valor médio dos produtos nas lojas.
+SELECT
+    AVG("preco") AS "precoMedio",
+    "nome"
+FROM
+    "ProdutoLoja"
+LEFT JOIN
+    "Produto"
+ON
+    "ProdutoLoja"."codProduto"="Produto"."codProduto"
+GROUP BY
+    "Produto"."codProduto";
